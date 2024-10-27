aa81e59a18c328ae8e3f3239caad913765f65d63
Kyle-Russ04 <kylejrussouw@gmail.com>
2024-10-27 19:06:04 +0000

    made game

--- Assets/Scripts/Cat Stuff/CatthuluMovement.cs
+++ Assets/Scripts/Cat Stuff/CatthuluMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatthuluMovement : MonoBehaviour
 {
@@ -15,11 +16,7 @@ public class CatthuluMovement : MonoBehaviour
     public AudioSource source;
     public AudioClip clip;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
@@ -61,7 +58,7 @@ public class CatthuluMovement : MonoBehaviour
             Debug.Log("Hit the cat");
             Debug.Log("Loose a life");
             cat2.lives -= 1f;
-
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
         }
     }
 }
