f34141efe1158ea9ad94a9de2ea03a5ab74ffcc5
Wxmble18 <seaashbury@gmail.com>
2024-10-27 01:49:51 +0000

    Added end screen to main project

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class EndScreen : MonoBehaviour
+{
+    public void FinishGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);  
+    }
+
+  
+}
